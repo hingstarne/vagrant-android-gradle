f3fee67de7c93c551e826879b75b2019d6ed8631
Arne-Kristian Hingst <hingst@tum.de>
2013-08-27 16:53:13 +0200

    Plain Release just r10k install

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-yum -y update
-yum install -y -q git
+# Update repository
+apt-get -q update
+
+# Install R10K dependencies
+apt-get install -y -q git ruby1.9.1
 
 if [[ "$(gem query -i -n r10k)" == "false" ]]; then
   gem install --no-rdoc --no-ri r10k
 fi
 
+# Build Puppet environment from Puppetfile
 cd /vagrant && r10k puppetfile install
-
-# Put in a brief wait before running the Puppet provisioner to clear yum activity
 sleep 5
